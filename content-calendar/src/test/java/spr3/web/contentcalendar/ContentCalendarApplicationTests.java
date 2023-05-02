7921aebe09d00072e2e5631750055810db4b29dd
john russelle domingo <johnrusselledomingo@gmail.com>
2023-05-02 17:00:36 +0800

    git intial

---
+++ content-calendar/src/test/java/spr3/web/contentcalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package spr3.web.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
