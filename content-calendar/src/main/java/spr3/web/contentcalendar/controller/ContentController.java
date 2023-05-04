a17dcc15ad7c572c6073ca33e3d7bdbfbb8d93e0
john russelle domingo <johnrusselledomingo@gmail.com>
2023-05-04 15:54:04 +0800

    add new

--- content-calendar/src/main/java/spr3/web/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/spr3/web/contentcalendar/controller/ContentController.java
@@ -1,12 +1,14 @@
 package spr3.web.contentcalendar.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spr3.web.contentcalendar.model.Content;
 import spr3.web.contentcalendar.repository.ContentCollectionRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("v1/api/content")
@@ -19,7 +21,15 @@ public class ContentController {
 
     @GetMapping("")
     public List<Content> findAll(){
+
         return repository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Content> findById(@PathVariable Integer id){
+
+        return repository.findById(id);
+    }
+
 
 }
